1d8676e93ae10c6ffbbd7c52b07044e8ded2cfce
basiston <amarbasiston@yahoo.in>
2017-09-21 11:40:36 +0200

    added javascript file(anguler controller and services files)

---
+++ RandomPasswordWeb/RandomPasswordWeb/Scripts/app/controllers/randomPasswordController.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+app.controller('randomPasswordController', ['$scope', 'randomPasswordService', function ($scope, randomPasswordService) {
+
+
+    $scope.Password =
+    {
+        value: ''
+    }
+
+    $scope.GeneratePassword = function (value) {
+        if (value === "") {
+            alert("value is null");
+            return;
+        }
+        randomPasswordService.getrandomPassword(true, true, true, true, 5).then(function (result) {
+            $scope.result = result.data;
+        }, function (error) {
+            $scope.error = error.statusText;
+        });
+
+    };
+
+    //$scope.Clear = function () {
+    //    $scope.Fizzbuzz = null;
+    //    $scope.result = null;
+
+    //};
+
+
+}]);
---
+++ RandomPasswordWeb/RandomPasswordWeb/Scripts/app/services/randomPasswordService.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+app.factory('randomPasswordService', ['$http', function ($http) {
+
+    var randomPasswordServiceFactory = {};
+
+    var _getrandomPassword = function (includeUpperCase, includeLowerCase, includeNumbers, includeSpecialChars, stringlength) {
+
+        return $http({
+            method: "GET",
+            url: "/api/RandomPasswordApi/",
+            params: {
+                includeUpperCase:includeUpperCase,
+                includeLowerCase:includeLowerCase,
+                includeNumbers:includeNumbers,
+                includeSpecialChars:includeSpecialChars,
+                stringlength:stringlength
+            }
+
+        })
+              .then(function mySucess(response) {
+                  return response;
+              });
+
+    };
+
+    randomPasswordServiceFactory.getrandomPassword = _getrandomPassword;
+
+    return randomPasswordServiceFactory;
+}]);
